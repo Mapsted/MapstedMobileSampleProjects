6545d912e7ddac81ef5a3c352fbdaf920ca8c2f0
kittu.monga <kritika.monga@mapsted.com>
2024-09-19 20:38:11 +0530

    Added Sample App project, with, Core Sdk, Sdk Map ,Sdk MapUI

--- Android/Sample_Kotlin_Compose/settings.gradle.kts
+++ Android/Sample_Kotlin_Compose/settings.gradle.kts
@@ -13,18 +13,7 @@ dependencyResolutionManagement {
         maven {
             url = uri("https://jitpack.io")
         }
-        maven {
-            url = uri("http://mobilesdk.mapsted.com:8081/artifactory/gradle-mapsted-developer")
-            isAllowInsecureProtocol = true
-            credentials {
-                username = "admin"
-                password = "LkSQk27jeUt86MJk"
-            }
-        }
-//        maven {
-//            url = uri("http://mobilesdk.mapsted.com:8081/artifactory/gradle-mapsted-developer")
-//        }
-//        maven { url = uri("https://mobilesdk.mapsted.com:8443/artifactory/gradle-mapsted") }
+        maven { url = uri("https://mobilesdk.mapsted.com:8443/artifactory/gradle-mapsted") }
     }
 }
 
